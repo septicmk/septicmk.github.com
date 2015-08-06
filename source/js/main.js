0c1787ff8a5975e7462f75df5facfb4c51828508
septicmk <septic.mk@gmail.com>
2015-08-06 10:40:09 +0800

    20150806

--- source/js/main.js
+++ source/js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 	
 	$(window).scroll(function(){  //只要窗口滚动,就触发下面代码 
         var scrollt = document.documentElement.scrollTop + document.body.scrollTop; //获取滚动后的高度 
-        if( scrollt > 400){  //判断滚动后高度超过200px,就显示
+        if( scrollt > 200){  //判断滚动后高度超过200px,就显示
             $("#gotop").fadeIn(400); //淡出
 			$(".navbar").stop().fadeTo(400, 0.2);
         }else{
@@ -29,7 +29,7 @@ $(document).ready(function() {
 	});
     $(".navbar").mouseleave(function(){
 		var scrollt = document.documentElement.scrollTop + document.body.scrollTop;
-		if ( scrollt > 400) {
+		if ( scrollt > 200) {
 			$(".navbar").fadeTo(100, 0.2);
 		}
 	});	
