2ea737486345820bf51c9abe74221029c3b8d852
septicmk <septic.mk@gmail.com>
2015-08-07 20:24:46 +0800

    20150807

--- source/js/main.js
+++ source/js/main.js
@@ -33,6 +33,12 @@ $(document).ready(function() {
 			$(".navbar").fadeTo(100, 0.2);
 		}
 	});
+    $('#wikiTOC').affix({
+        offset: {
+            bottom: ($('#disqus_thread').outerHeight(true) + 
+                    $('.container-narrow').outerHeight(true)) + 
+                    40
+    }
 });    
 	setTitleDate();
 });
